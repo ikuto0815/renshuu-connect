f8de29dc5e1e8e54295b4074fd5f88ebe6d24ea1
Ikuto <ikuto0815@proton.me>
2025-02-21 22:03:31 +0100

    fix build for windows, add icons
    some windows only dependencies were missing..

--- renshuu_connect.py
+++ renshuu_connect.py
@@ -176,19 +176,7 @@ if os.name == 'nt':
     from pystray import Icon as icon, Menu as menu, MenuItem as item
     from PIL import Image, ImageDraw
     import psutil
-
-    def create_image(width, height, color1, color2):
-        # Generate an image and draw a pattern
-        image = Image.new('RGB', (width, height), color1)
-        dc = ImageDraw.Draw(image)
-        dc.rectangle(
-            (width // 2, 0, width, height // 2),
-            fill=color2)
-        dc.rectangle(
-            (0, height // 2, width // 2, height),
-            fill=color2)
-
-        return image
+    import pkgutil
 
     def on_clicked(icon, item):
         icon.stop()
@@ -199,7 +187,7 @@ if os.name == 'nt':
         parent.kill()
         sys.exit(0)
 
-    icon('test', create_image(64, 64, 'black', 'white'), menu=menu(
+    icon('test', Image.open(os.path.dirname(__file__) + '/kao.png'), menu=menu(
         item(
             'Exit',
             on_clicked
